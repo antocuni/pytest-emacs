c830c2c5090f33e39355a077ddfc6a55472603dd
Antonio Cuni <anto.cuni@gmail.com>
2011-10-05 13:38:42 +0200

    make the buffer read-only when the process finishes, and bind q to quit-window

--- pytest.el
+++ pytest.el
@@ -11,6 +11,12 @@
 
 (defconst pytest-def-re "def \\(test_[A-Za-z0-9]+\\)")
 
+(defun pytest-term-sentinel (proc msg)
+  (term-sentinel proc msg)
+  (when (memq (process-status proc) '(signal exit))
+    (setq buffer-read-only t)
+    (local-set-key "q" 'quit-window)))
+
 (defun pytest-run (cmdline show-prompt)
   (let ((cmdline (if show-prompt
                      (read-shell-command "Run: " cmdline
@@ -20,7 +26,10 @@
     (switch-to-buffer-other-window buffer)
     (insert cmdline)
     (newline)
-    (term-ansi-make-term "*pytest*" "/bin/sh" nil "-c" cmdline)))
+    (term-ansi-make-term "*pytest*" "/bin/sh" nil "-c" cmdline)
+    (let ((proc (get-buffer-process buffer)))
+      ; override the default sentinel set by term-ansi-make-term
+      (set-process-sentinel proc 'pytest-term-sentinel))))
 
 
 (defun pytest-arg-from-buffer-name (buffer-name)
